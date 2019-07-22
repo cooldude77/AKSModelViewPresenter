7adf0285131fa8611f577d630daedf86bf19854b
Ashvini Kumar Saxena <ashvinisaxena77@gmail.com>
2019-07-22 15:06:24 +0530

    Updating Presenter Network Reposne Interface

--- aks-mvp/src/main/java/com/instanect/aks_mvp/mvp/presenter/AbstractNetworkPresenter.java
+++ aks-mvp/src/main/java/com/instanect/aks_mvp/mvp/presenter/AbstractNetworkPresenter.java
@@ -2,27 +2,14 @@ package com.instanect.aks_mvp.mvp.presenter;
 
 import com.instanect.aks_mvp.mvp.interactors.network.interfaces.NetworkInteractorResponseInterface;
 import com.instanect.aks_mvp.mvp.presenter.interfaces.PresenterNetworkResponseInterface;
-import com.instanect.networkcommon.NetworkResponseInterface;
 
-public class AbstractNetworkPresenter extends AbstractPresenter implements NetworkInteractorResponseInterface {
+public abstract class AbstractNetworkPresenter extends AbstractPresenter implements NetworkInteractorResponseInterface {
 
     @Override
     public PresenterNetworkResponseInterface getPresenterResponseInterface() {
         return (PresenterNetworkResponseInterface) super.getPresenterResponseInterface();
     }
 
-    @Override
-    public <T> void onNetworkInteractorCallSuccessful(NetworkResponseInterface<T> networkResponse) {
-
-        getPresenterResponseInterface().onNetworkCallSuccess((String) networkResponse.getResponse());
-
-    }
-
-    @Override
-    public void onNetworkInteractorScenarioCallFailure(String failureMessages, int errorCode) {
-
-        getPresenterResponseInterface().onNetworkCallFailure(failureMessages, errorCode);
-    }
 
     @Override
     public void onInternetNotAvailable() {
